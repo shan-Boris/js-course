99f6af707841da6a1d0951bc764b22d319e2acfc
shan-Boris <shanboris@gmail.com>
2022-02-12 19:39:25 +0700

    step 2

--- application.js
+++ application.js
@@ -5,6 +5,15 @@ function promptNumb(str) {
     while (numb == null || numb == '' || isNaN(numb)) {numb = prompt(str, "");};
     return +numb;
 }
+
+function checkPromptStr(str) {
+    let val = prompt(str, '');
+    while (val == null || val.length < 3 || val.length > 50) {
+        val = prompt(str, '');
+    };
+    return val;
+
+}
 const numberOfFilms = promptNumb("Сколько фильмов вы уже посмотрели?");
 
 
@@ -19,9 +28,12 @@ const personalMovieDB = {
 let lastFilm, rating;
 
 for (let i = 0; i < 2; i++ ) {
- lastFilm = prompt ("Один из последних просмотренных фильмов?","");
+ lastFilm = checkPromptStr("Один из последних просмотренных фильмов?");
  rating = promptNumb ("На сколько оцените его?")
 ;
     personalMovieDB.movies[lastFilm] = rating;
 };
 
+if (personalMovieDB.count < 10) {alert("Просмотрено довольно мало фильмов")}
+else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {alert("Вы классический зритель")}
+else if (personalMovieDB.count >= 30) {alert("Вы киноман")};
