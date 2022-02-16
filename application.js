82d78b14fa253f2f3b71a59a88c9e4515742714b
shan-Boris <shanboris@gmail.com>
2022-02-16 21:23:16 +0700

    'step4'

--- application.js
+++ application.js
@@ -14,41 +14,48 @@ function checkPromptStr(str) {
     return val;
 
 }
-const numberOfFilms = promptNumb("Сколько фильмов вы уже посмотрели?");
 
 
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: () => {
+        this.count = promptNumb("Сколько фильмов вы уже посмотрели?")
+    },
+    rememberMyFilms: () => {
+        let lastFilm, rating;
+
+        for (let i = 0; i < 2; i++ ) {
+        lastFilm = checkPromptStr("Один из последних просмотренных фильмов?");
+        rating = promptNumb ("На сколько оцените его?")
+        ;
+            personalMovieDB.movies[lastFilm] = rating;
+        };
+    },
+    detectPersonalLevel: () => {
+        
+        if (personalMovieDB.count < 10) {alert("Просмотрено довольно мало фильмов")}
+        else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {alert("Вы классический зритель")}
+        else if (personalMovieDB.count >= 30) {alert("Вы киноман")};
+    },
+    showMyDB: function() {
+        if (!personalMovieDB.privat) {console.log(personalMovieDB)}
+        },
+    writeYourGenres: function() {
+        for (let i = 1; i < 4; i++) {
+            personalMovieDB.genres.push(checkPromptStr(`Ваш любимый жанр под номером ${i}`));
+        };
+        this.genres.forEach((value, index) => alert(`Любимый жанр #${index + 1} - это ${value}`))
+        },
+    toggleVisibleMyDB: function() {
+        if(this.privat) this.privat = false;
+        else this.privat = true;
+    }
+
 };
 
-let lastFilm, rating;
 
-for (let i = 0; i < 2; i++ ) {
- lastFilm = checkPromptStr("Один из последних просмотренных фильмов?");
- rating = promptNumb ("На сколько оцените его?")
-;
-    personalMovieDB.movies[lastFilm] = rating;
-};
-
-
-
-if (personalMovieDB.count < 10) {alert("Просмотрено довольно мало фильмов")}
-else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {alert("Вы классический зритель")}
-else if (personalMovieDB.count >= 30) {alert("Вы киноман")};
-
-function showMyDB() {
-    if (!personalMovieDB.privat) {console.log(personalMovieDB)}
-}
-
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++) {
-        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
-    };
-};
-
-writeYourGenres();
