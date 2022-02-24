2114bddce72bea9d19c223ba471b881aa18828a8
shan-Boris <shanboris@gmail.com>
2022-02-24 20:56:46 +0700

    'Add_timer'

--- Food_project/js/script.js
+++ Food_project/js/script.js
@@ -34,4 +34,52 @@ window.addEventListener('DOMContentLoaded', () => {
     }));
     hideTabsContent();
     showTabContent();
+
+    // timer
+
+    const deadline = 'February 25, 2022';
+
+    function getTimeOff(endtime) {
+        const delta = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(delta / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((delta / (1000 * 60 * 60) % 24) ),
+            min = Math.floor((delta / (1000 * 60)) % 60),
+            sec = Math.floor((delta / 1000 % 60) );
+
+        return {
+            'total': delta,
+            'days': days,
+            'hours': hours,
+            'minutes': min,
+            'seconds': sec
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            clockId = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock(){
+        const t = getTimeOff(endtime);
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+        
+        if(t.total <= 0) clearInterval(clockId);
+
+        }
+
+        function getZero(number) {
+            return number < 10 ? `0${number}` : number
+        }
+    }
+
+    setClock('.timer', deadline);
 });
