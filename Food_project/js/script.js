03f1ba4ca0cde73db538b8f58c5be442884695ef
shan-Boris <shanboris@gmail.com>
2022-02-28 21:51:22 +0700

    Work with class

--- Food_project/js/script.js
+++ Food_project/js/script.js
@@ -126,6 +126,67 @@ window.addEventListener('DOMContentLoaded', () => {
     }};
 
     document.addEventListener('scroll', showModalInScrollEnd);
+
+    // class
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.trans = 27;
+            this.changeToUAH();
+            this.parent = document.querySelector('.menu__field > .container');
+        }
+
+        
+        changeToUAH() {
+            this.price *= this.trans;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `<div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>`
+
+            this.parent.append(element);
+        }
+    }
+
+    document.querySelector('.menu__field > .container').innerHTML = '';
+    new MenuCard(
+        'img/tabs/vegy.jpg', 
+        'vegy', 
+        'Меню "Фитнес"', 
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
+        9)
+    .render();
+
+    new MenuCard(
+        'img/tabs/post.jpg', 
+        'post', 
+        'Меню "Постное"', 
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 
+        15)
+    .render();
+
+    new MenuCard(
+        'img/tabs/elite.jpg', 
+        'elite', 
+        'Меню “Премиум”', 
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
+        20)
+    .render();
 });
 
 
