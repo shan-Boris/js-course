394af755af1263d7f577815caee1c1de3099f375
shan-Boris <shanboris@gmail.com>
2022-03-16 22:17:23 +0700

    Add navigation panel at slider

--- Food_project/js/script.js
+++ Food_project/js/script.js
@@ -253,16 +253,25 @@ window.addEventListener('DOMContentLoaded', () => {
     const prevSlide = document.querySelector('.offer__slider-prev'),
     nextSlide = document.querySelector('.offer__slider-next'),      
     currentSlideNumb = document.querySelector('#current'),
-    sliderInner = document.querySelector('.offer__slider-inner');
+    sliderInner = document.querySelector('.offer__slider-inner'),
+    wrapperSlides = document.querySelector('.offer__slider-wrapper'),
+    slideNav = document.createElement('div');
+
+    wrapperSlides.style.cssText = 'overflow: hidden; position: relative;';
+    wrapperSlides.append(slideNav);
+    slideNav.classList.add('carousel-indicators');
 
     function addSlideInHTML(slideData, i) {
         const slide = document.createElement('div');
         slide.classList.add('offer__slide');
         slide.innerHTML = `<img src=${slideData.img} alt=${slideData.altimg}>`;
         sliderInner.append(slide);
+        slideNav.innerHTML += `<li id = slide${i} class = "dot"> </li>`;
     }
     let slides,
-        widthInner;
+        widthInner,
+        navDots;
+
     getData('http://localhost:3000/slider').then(slide => {
         slide.forEach((slide, i) => {
            addSlideInHTML(slide, i);
@@ -273,38 +282,51 @@ window.addEventListener('DOMContentLoaded', () => {
         totalSlide.textContent = addZero(slide.length);
     }).then(() => {
         slides = document.querySelectorAll('.offer__slide');
-        widthInner = window.getComputedStyle(sliderInner).width
+        widthInner = window.getComputedStyle(sliderInner).width;
+        navDots = document.querySelectorAll('.dot');
+        navDots[pos].style.cssText = 'opacity: 1;';
     });
 
-    let currentSlide = 1;
-    document.querySelector('.offer__slider-wrapper').style.cssText = 'overflow: hidden;';
+
     sliderInner.style.display = 'flex';
     sliderInner.style.transition = '0.5s all';
 
     let pos = 0;
-    nextSlide.addEventListener('click', (e) => {
-        if (pos !== -parseInt(widthInner) + parseInt(widthInner)/slides.length) {
-            pos -= parseInt(widthInner)/slides.length;
-            currentSlideNumb.textContent = addZero(+currentSlideNumb.textContent + 1);
-        } else {
-            pos = 0;
-            currentSlideNumb.textContent = '01';
-        }
-        sliderInner.style.transform = `translate(${pos}px, 0)`;
 
+    function changeSlide(changePos) {
+        navDots[pos].style.cssText = '';
+        changePos();
+        currentSlideNumb.textContent = addZero(pos + 1);
+        sliderInner.style.transform = `translate(${pos * -parseInt(widthInner)/slides.length}px, 0)`;
+        navDots[pos].style.cssText = 'opacity: 1;';
+    }
+
+    nextSlide.addEventListener('click', () => {
+        function changePos () {
+            pos !== slides.length - 1 ? pos += 1 : pos = 0;
+        };        
+        changeSlide(changePos)
     })
     
-    prevSlide.addEventListener('click', (e) => {
-        if (pos !== 0 ) {
-            pos += parseInt(widthInner)/slides.length;
-            currentSlideNumb.textContent = addZero(+currentSlideNumb.textContent - 1);
-        } else {
-            pos = -parseInt(widthInner) + parseInt(widthInner)/slides.length;
-            currentSlideNumb.textContent = addZero(slides.length);
-        }
-        sliderInner.style.transform = `translate(${pos}px, 0)`;
-
+    
+    prevSlide.addEventListener('click', () => {
+        function changePos () {
+            pos !== 0 ? pos -= 1 : pos = slides.length - 1;
+        };        
+        changeSlide(changePos)
     })
+
+
+    function clickSlideNav(target) {
+        if (target.tagName == 'LI') {
+            function changePos () {
+                pos = +target.id[5];
+            };        
+            changeSlide(changePos)
+        }  
+    };
+    slideNav.addEventListener('click', (e) => clickSlideNav(e.target));
+
 
 
 });
